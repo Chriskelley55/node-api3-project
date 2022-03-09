af641d5271ce372984ad9097f3b2e70c8a0aee25
Chris Kelley <chriskelley85@gmail.com>
2022-03-09 16:10:57 -0600

    added logger, get all

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,9 +1,25 @@
+const Users = require('../users/users-model');
+
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`)
+next();
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  const { id } = req.params;
+
+  Users.findById(id)
+    .then(result => {
+      if (result === null || result === undefined) {
+        res.status(404).json({ message: "user not found" }  );
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    })
 }
 
 function validateUser(req, res, next) {
@@ -15,3 +31,9 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+};
